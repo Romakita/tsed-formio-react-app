b94407792a2a6ff38b0e8733f5ca8b0f990e7b1b
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-10-30 14:35:06 +0200

    feat: Configure app to run formbuilder without styling

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {FormBuilder} from "@tsed/react-formio";
+
+function App() {
+  return (
+    <div className="App">
+      <FormBuilder display={'form'} components={[]} />
+    </div>
+  );
+}
+
+export default App;
